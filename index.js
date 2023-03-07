c69e6e7cc0f53c75f866b12c6df8ebb37f67ab7e
Jai <jaijai.stell@gmail.com>
2023-03-07 15:40:37 -0700

    completed lab yay

---
+++ index.js
@@ -0,0 +1,12 @@
+function introduction(name){
+    return `Hi, my name is ${name}.`;
+}
+//ahhhhhhh Jai's work
+function introductionWithLanguage(name, language){
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+  }
+//I am sooooooo bad at the syntax for JS
+function introductionWithLanguageOptional(name, language ="JavaScript"){
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+  }
+  //yay they all passed!
